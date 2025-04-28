fed1bba8c28ba8f1701a13eccae21877cf7bbe85
The Gaming Engineer <gaming_engineer@outlook.com>
2025-04-28 14:09:11 +0200

    opdatering den 28/4-2025
    initielt pytorch opgave

--- uge_10-11 - machine learning/src/main.py
+++ uge_10-11 - machine learning/src/main.py
@@ -7,15 +7,62 @@ Created on Mon Apr 28 11:10:22 2025
 
 import os
 from dotenv import load_dotenv, dotenv_values
-
+import torchvision
+from torch.utils.data import DataLoader, Subset
+from torch.accelerator import current_accelerator, is_available
+from torch.optim import SGD
+import torch.nn as nn
+from network import neural_network
 
 load_dotenv()
 
-print(os.getenv("Epochs"))
-print(os.getenv("Learning_rate"))
-print(os.getenv("Batch_size"))
+root="..\data"
+transformation=torchvision.transforms.ToTensor()
+targeted_transformation=None
+downloaded=False
+
+
+Epochs = int(os.getenv("Epochs"))
+Learning_rate = float(os.getenv("Learning_rate"))
+Batch_size = int(os.getenv("Batch_size"))
+    
+    
+def subset_loader(root, train, transform, target_transform,download, size, shuffle=True):
+    data=torchvision.datasets.FashionMNIST(root = root, 
+                                           train = train, 
+                                           transform = transform, 
+                                           target_transform = target_transform, 
+                                           download= download)
+    
+    subset_dataset = Subset(data,list(range(len(data))))
+    
+    return DataLoader(subset_dataset, batch_size=size, shuffle=shuffle)
+    
 
 
 
+train_dataset = subset_loader(root = root, 
+                              train = True, 
+                              transform = transformation, 
+                              target_transform = targeted_transformation, 
+                              download = downloaded,
+                              size = Batch_size)
+
+test_dataset = subset_loader(root = root, 
+                             train = False, 
+                             transform = transformation, 
+                             target_transform = targeted_transformation, 
+                             download = downloaded,
+                             size = Batch_size)
+
+
+
+device = current_accelerator().type if is_available() else "cpu"
+
+model = neural_network.to(device)
+
+optimizer = SGD(params = model.parameters(), lr = Learning_rate)
+
+loss_fn = nn.CrossEntropyLoss()
 
 
---
+++ uge_10-11 - machine learning/src/network.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 28 13:28:12 2025
+
+@author: spac-30
+"""
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class neural_network(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.network_stack = nn.Sequential(
+            nn.Linear(in_features = 28*28, out_features = 512),
+        nn.ReLU(),
+        nn.Linear(in_features = 512, out_features = 10)
+        )
+        
+    
+    def forward(self,x):
+        x = self.flatten()
+        output = self.network_stack(x)
+        return output
+    
