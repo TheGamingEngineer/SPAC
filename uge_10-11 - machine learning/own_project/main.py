28ac13e1784deb3d3485fbcc32210a6b56337aec
The Gaming Engineer <gaming_engineer@outlook.com>
2025-05-09 14:45:11 +0200

    prokaryot datasæt

--- uge_10-11 - machine learning/own_project/main.py
+++ uge_10-11 - machine learning/own_project/main.py
@@ -29,7 +29,28 @@ model = neural_network().to(device)
 
 promoter_Data=pd.read_csv("promoters_eukaryot_2.csv",sep=";")    
 
-
+class MLP(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.network_stack = nn.Sequential(
+        nn.Linear(in_features = 31812, out_features = 612),
+        nn.BatchNorm1d(612), # nromaliser aktivering
+        nn.ReLU(), # tilføjer non-linearitet
+        nn.Linear(in_features = 612, out_features = 410),
+        nn.BatchNorm1d(410), # nromaliser aktivering
+        nn.ReLU(), # tilføjer non-linearitet
+        nn.Linear(in_features = 410, out_features = 210),
+        nn.BatchNorm1d(210), # nromaliser aktivering
+        nn.ReLU(), # tilføjer non-linearitet
+        nn.Linear(in_features = 210, out_features = 5)
+        )
+        
+    
+    def forward(self,x):
+        x = self.flatten(x)
+        output = self.network_stack(x)
+        return output
 
 
 
